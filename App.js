e25bac2c054d343b0815bacccf9a4909573fc24c
João Dionisio <jbdionisio@hotmail.com.br>
2020-06-04 13:58:40 -0300

    working with modal

--- App.js
+++ App.js
@@ -1,29 +1,35 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
+import { StyleSheet, View, Button, FlatList } from 'react-native';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
 
 export default function App() {
-
   const [dayGoals, setDayGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
 
   const addGoalHandler = goalTitle => {
     setDayGoals(currentGoals => [
       ...currentGoals,
       { id: Math.random().toString(), value: goalTitle }
     ]);
+    setIsAddMode(false);
   };
 
   const removeGoalHandler = goalId => {
     setDayGoals(currentGoals => {
       return currentGoals.filter((goal) => goal.id !== goalId);
     })
-  }
+  };
+
+  const cancelGoalHandler = () => {
+    setIsAddMode(false);
+  };
 
   return (
     <View style={styles.screen}>
-      <GoalInput onAddGoal={addGoalHandler} />
+      <Button title={'Add new goal'} onPress={() => setIsAddMode(true)} />
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={dayGoals}
